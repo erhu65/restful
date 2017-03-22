72f5717b0bc4760cc2a79f917b92aa82f944d526
peter huang <erhu65@gmail.com>
2017-03-22 21:31:44 +0800

    <<EOF 7. Key/value store - LevelDB,   video_nodes/27/71 EOF

--- app.js
+++ app.js
@@ -1,55 +1,70 @@
-/**
- * Module dependencies.
- */
 
-var express = require('express');
-var path = require('path');
-
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var contacts = require('./modules/contacts');
-var http = require('http');
-var url = require('url');
-
-var app = express();
-
-app.get('/contacts',
-    function(request, response){
-        var get_params = url.parse(request.url, true).query;
-
-        if (Object.keys(get_params).length === 0)
-        {
-            response.setHeader('content-type', 'application/json');
-            response.end(JSON.stringify(contacts.list()));
-        }
-        else
-        {
-            response.setHeader('content-type', 'application/json');
-            response.end(JSON.stringify(contacts.query_by_arg(get_params.arg, get_params.value)));
-        }
-    }
-);
+	/**
+	 * 
+	 dependencies.
+	 */
 
 
-app.get('/contacts/:number', function(request, response) {
-    response.setHeader('content-type', 'application/json');
-    response.end(JSON.stringify(contacts.query(request.params.number)));
-});
 
-app.get('/groups', function(request, response) {
-    console.log ('groups');
-    response.setHeader('content-type', 'application/json');
-    response.end(JSON.stringify(contacts.list_groups()));
-});
+	var express = require('express')
+	  , http = require('http')
+	  , path = require('path')
+	  , bodyParser = require('body-parser')
+	  , logger = require('morgan')
+	  , methodOverride = require('method-override')
+	  , errorHandler = require('errorhandler')
+	  , levelup = require('levelup');
+	var app = express();
+	var url = require('url');
 
-app.get('/groups/:name', function(request, response) {
-    console.log ('groups');
-    response.setHeader('content-type', 'application/json');
-    response.end(JSON.stringify(contacts.get_members(request.params.name)));
-});
+	// all environments
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
 
+	app.use(methodOverride());
+	app.use(bodyParser.json());
+	// development only
+	if ('development' == app.get('env')) {
+	  app.use(errorHandler());
+	}
 
-http.createServer(app).listen(3000, function(){
-    console.log('Express server listening on port 3000');
-});
+	var db = levelup('./contacts',  {valueEncoding: 'json'});
+	db.put('+359777123456', {
+			"firstname": "Joe",
+			"lastname": "Smith",
+			"title": "Mr.",
+			"company": "Dev Inc.",
+			"jobtitle": "Developer",
+			"primarycontactnumber": "+359777123456",
+			"othercontactnumbers": [
+				"+359777456789",
+				"+359777112233"],
+			"primaryemailaddress": "joe.smith@xyz.com",
+			"emailaddresses": [
+				"j.smith@xyz.com"],
+			"groups": [
+				"Dev",
+				"Family"]
+		});
+
+	app.get('/contacts/:number', function(request, response) {	
+
+		console.log(request.url + ' : querying for ' + request.params.number);
+		db.get(request.params.number, function(error, data) {
+
+						if (error) {
+							response.writeHead(404, {
+								'Content-Type' : 'text/plain'});
+							response.end('Not Found');
+							return;
+						} 
+						
+						response.setHeader('content-type', 'application/json');	
+						response.send(data);
+					  });
+	});
+
+	console.log('Running at port ' + app.get('port'));
+	http.createServer(app).listen(app.get('port'));
+
